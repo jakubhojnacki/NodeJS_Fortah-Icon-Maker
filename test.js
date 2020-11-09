39a9c432097f6bf270e3dd78343aa559f937a489
Jakub Hojnacki <jakubhojnacki@gmail.com>
2020-11-09 10:38:12 +0000

    Folder and File icons

--- test.js
+++ test.js
@@ -1,9 +1,10 @@
 (function () {
-	const templatesFolderPath = ".\\Templates";
+	const subfolder = "Files";
+	const templatesFolderPath = `.\\${subfolder}\\Templates`;
 	const symbolsFolderPath = ".\\Symbols";
-	const iconsFolderPath = ".\\Icons";
+	const iconsFolderPath = `.\\${subfolder}\\Icons`;
 	const temporaryFolderPath = ".\\Temp";
-	const settingsPath = ".\\iconMakerSettings.json";
+	const settingsPath = `.\\${subfolder}\\iconMakerSettings.json`;
 	process.argv = [process.argv[0], process.argv[1], templatesFolderPath, symbolsFolderPath, iconsFolderPath, temporaryFolderPath, settingsPath];
 	require(".\\iconMaker.js");
 })();
